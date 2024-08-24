6607be8cec363cdbcb6e1a65639724a06dd031c5
Tom James <63594225+ammojest@users.noreply.github.com>
2024-08-24 19:43:58 +0100

    chore: Update GitHub page with guide for adding a new local directory to a new GitHub repo

--- src/app/guides/github/page.tsx
+++ src/app/guides/github/page.tsx
@@ -3,10 +3,18 @@ import React from "react";
 const github = () => {
   return (
     <>
-      <div>Github Page</div>
+      <div className="flex justify-center items-center py-5">
+        <h1>Guides: Github</h1>
+      </div>
       <div>
         <h2>Guide: Adding a new local directory to a new Github repo</h2>
-        <p></p>
+        <ul>
+          <li>git init</li>
+          <li>git remote add origin</li>
+          <li>git add .</li>
+          <li>git commit -m &quot;Initial commit&quot;</li>
+          <li>git push -u origin main</li>
+        </ul>
       </div>
     </>
   );
