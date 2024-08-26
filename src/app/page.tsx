f87150c6eff35beac631a9b3391affc06fa4cea2
Tom James <63594225+ammojest@users.noreply.github.com>
2024-08-26 15:09:42 +0100

    homepage holding content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,32 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <main>main goes here</main>;
+  return (
+    <div className="flex justify-center min-h-screen">
+      <main className="w-full max-w-7xl">
+        <div className="flex justify-center flex-col items-center my-5">
+          <h1>Home</h1>
+          <p>Welcome to the home page.</p>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <Link href="/guides/react" className="underline">
+                React
+              </Link>
+            </li>
+            <li>
+              <Link href="/guides/google-analytics">Google Analytics</Link>
+            </li>
+            <li>
+              <Link href="/guides/next">Next JS</Link>
+            </li>
+            <li>
+              <Link href="/guides/react/components">React Components</Link>
+            </li>
+          </ul>
+        </div>
+      </main>
+    </div>
+  );
 }
